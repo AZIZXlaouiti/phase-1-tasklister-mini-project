b864ae11523497dcb7f410d6a8d53f95525ff954
AZIZXlaouiti <azizrocke@gmail.com>
2021-06-30 10:00:56 -0400

    Completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
+const tasklist=[]
+
+
+
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const le = document.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    const toDo = document.querySelector('#new-task-description')
+      if (toDo.value !== ""){
+        
+        tasklist.push(toDo.value)
+        let li = document.createElement('li')
+        let bu = document.createElement('button')
+        bu.innerHTML = 'X'
+        
+        li.innerHTML = toDo.value
+        let item = document.querySelector('#list #tasks')
+        console.log(tasklist)
+        item.appendChild(li)
+        li.appendChild(bu)
+        // e.target.reset()
+        toDo.value = ''
+      }
+    
+     
+      
+
+    
+   
+  });
+  
 });
